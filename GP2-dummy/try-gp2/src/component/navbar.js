065df4f41803d4a810220035aa37733c79c50dcb
Ganjar Widatama <gwidatama@gmail.com>
2022-09-11 21:17:42 +0700

    sorry ikut ngubah xixi

--- GP2-dummy/try-gp2/src/component/navbar.js
+++ GP2-dummy/try-gp2/src/component/navbar.js
@@ -1,42 +1,66 @@
-import React from 'react'
+import React from "react";
 import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
-import { Link } from 'react-router-dom';
-import '../style/navbar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import "../style/navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/logo.png";
 
 const NavbarNav = () => {
   return (
     <div>
-        <Navbar expand="lg" className='nav-top'>
-            <Container fluid>
-                <Navbar.Brand href="#home" style={{color: 'whitesmoke'}}>React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto d-flex gap-3">
-                    <Link className='text-decoration-none text-white ' to='/'>Home</Link>
-                    <Link to='/' className='text-decoration-none text-white'>Product</Link>
-                </Nav>
-                <Form className="d-flex">
-                    <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                    />
-                    <Button variant="secondary" style={{marginRight:'30px'}}>Search</Button>
-                    <Button variant='secondary' className='button-nav' style={{marginRight: '10px', backgroundColor: 'none'}}>
-                        <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
-                    </Button>
-                    <Button variant="secondary">
-                        <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
-                    </Button>
-                </Form>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+      <Navbar expand="lg" className="nav-top ">
+        <Container fluid>
+          <Navbar.Brand href="#home" style={{ color: "whitesmoke" }}>
+            <img src={logo} alt="horse" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto d-flex gap-3">
+              <Link
+                className="text-decoration-none text-white  "
+                style={{ marginLeft: "10vw", fontSize: "2vw" }}
+                to="/"
+              >
+                Home
+              </Link>
+              <Link
+                to="/"
+                className="text-decoration-none text-white"
+                style={{ fontSize: "2vw" }}
+              >
+                Product
+              </Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="secondary" style={{ marginRight: "30px" }}>
+                Search
+              </Button>
+              <Button
+                variant="secondary"
+                className="button-nav"
+                style={{ marginRight: "10px", backgroundColor: "none" }}
+              >
+                <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
+              </Button>
+              <Button variant="secondary">
+                <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarNav
+export default NavbarNav;
--- GP2-dummy/try-gp2/src/pages/Home.js
+++ GP2-dummy/try-gp2/src/pages/Home.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import Cards from '../component/card'
-import Footer from '../component/footer'
-import NavbarNav from '../component/navbar'
+import React from "react";
+import Cards from "../component/card";
+import Footer from "../component/footer";
+import NavbarNav from "../component/navbar";
 import { Tab, Tabs } from "react-bootstrap";
+import "../style/footer.css";
 
 const Home = () => {
   return (
     <div>
-        <NavbarNav />
+      <NavbarNav />
+      <div className="d-flex">
         <Cards />
         <Cards />
         <Cards />
         <Cards />
         <Cards />
         <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
