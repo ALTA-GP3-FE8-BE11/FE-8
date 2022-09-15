8f5c50d890fa2706a608b959075544d2e2136a08
Ganjar Widatama <gwidatama@gmail.com>
2022-09-15 15:26:12 +0700

    CRUD masih error

--- GP2-dummy/try-gp2/src/component/mod-prod.js
+++ GP2-dummy/try-gp2/src/component/mod-prod.js
@@ -1,74 +1,102 @@
-import React from 'react'
+import React from "react";
 import { Modal, Form, Button } from "react-bootstrap";
 
-const ModProd = ({ show, handleClose }) => {
-    return (
-        <div>
-            <Modal show={show} onHide={handleClose} centered style={{ width: '100%' }}>
-                <Modal.Header closeButton >
-                    <Modal.Title style={{ textAlign: 'center' }}>TAMBAH PRODUK</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control
-                                type="text"
-                                placeholder="Nama Produk"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control
-                                type="text"
-                                placeholder="Nama brand"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control
-                                type="number"
-                                placeholder="Ukuran"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control
-                                type="text"
-                                placeholder="Gender pengguna"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control
-                                type="number"
-                                placeholder="Harga"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Control
-                                type="text"
-                                placeholder="Url image"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlTextarea1"
-                        >
-                            <Form.Label>Deskripsi Produk</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleClose}>
-                        Add Product
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    )
-}
+const ModProd = ({
+  show,
+  handleSubmit,
+  handleClose,
+  handleNamaproduk,
+  handleBrand,
+  handleUkuran,
+  handleGender,
+  handleHarga,
+  handleGambar,
+  handleDeskripsi,
+}) => {
+  return (
+    <div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        style={{ width: "100%" }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title style={{ textAlign: "center" }}>
+            TAMBAH PRODUK
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="text"
+                placeholder="Nama Produk"
+                onChange={(value) => handleNamaproduk(value)}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="text"
+                placeholder="Nama brand"
+                onChange={(value) => handleBrand(value)}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="number"
+                placeholder="Ukuran"
+                onChange={(value) => handleUkuran(value)}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="text"
+                placeholder="Gender pengguna"
+                onChange={(value) => handleGender(value)}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="number"
+                placeholder="Harga"
+                onChange={(value) => handleHarga(value)}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="text"
+                placeholder="Url image"
+                onChange={(value) => handleGambar(value)}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Deskripsi Produk</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                onChange={(value) => handleDeskripsi(value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmit}>
+            Add Product
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
 
-export default ModProd
+export default ModProd;
